f299627241b26107a4c0893e232fa9ee103236cf
Red4th <andreabasilio.rava@studio.unibo.it>
2019-04-08 15:50:46 +0100

    Finished ListOfPrimes

--- ListOfPrimes.py
+++ ListOfPrimes.py
@@ -1,4 +1,14 @@
 def is_prime (x):
+    if x==0 :
+        return True
+    if x==1 :
+        return True
+    i=2
+    while i < x:
+       if x%i==0 :
+           return False
+       else: 
+           i += 1
     return True
 
 def find_primes (x):
@@ -9,4 +19,6 @@ def find_primes (x):
             primes.append(i)
         i += 1
     return primes
+
+print(find_primes (20))
     
