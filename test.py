550e32730553414f7875cc39e3904a6ab1a3c539
Red4th <andreabasilio.rava@studio.unibo.it>
2019-04-08 17:54:52 +0100

    try

---
+++ test.py
@@ -0,0 +1,10 @@
+#%%
+ID = dict (001 = dict ('nome' = 'Andrea', 'città' = 'Faenza ), 
+           002 = dict ('nome' = 'Tommaso', 'città' = 'Fidenza')
+          )
+#%%
+ID = {'001' : {'name' : 'Andrea', 'town' : 'Faenza'},
+      '002' : {'name' : 'Tommaso', 'town' : 'Fidenza'}
+     }
+ID.values()
+#%%
