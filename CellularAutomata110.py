082034ede08013e668602dfa5343cc79e192f0f0
Red4th <andreabasilio.rava@studio.unibo.it>
2019-04-09 13:04:46 +0100

    defined boundary_evolution()

--- CellularAutomata110.py
+++ CellularAutomata110.py
@@ -9,4 +9,21 @@ CA110 = { "..." : "0",
         }
 CA110.keys()
 CA110.values()
-#comment added
+
+my_state = [".",".",".",".",".",".",".",".",".","."]
+
+def boundary_evolution (state) :
+    if state[0] == "0" :
+        state[0] = "."
+    else :
+        state[0] = "0"
+    if state[-1] == "0" :
+        state[-1] = "."
+    else :
+        state[-1] = "0"  
+    return state
+        
+print(boundary_evolution(my_state))        
+        
+#def evolution (state) :
+        
